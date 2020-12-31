c2b61460dd14b90ecd5aeeea4c6ffeff130a4236
Eric Weiss <me@kinnybot.com>
2020-12-31 12:04:56 -0500

    is lit

---
+++ main.py
@@ -0,0 +1,89 @@
+print("""
+                                                      ...
+                           'cloooodddkxxxkkkkkkkxdc.ldlc:;,,;;,,,,....
+                        .cdxoooooooodOooooooooooooox:cO0kollcc:::;,,,ll;.
+                     .'ldolooooooolodxlllllllllllllldo;KWXkdlcc::;;,',okXKl.
+      ':::,,,,,,'....cdollllllccclclxocccccccccc:,,,,:l,xWNKxoolcc::;,,,cOWNx,
+       ...:;,,,;...,xlcccc::::::::::d:::::::::::,......l,cKX0kxkkkkkxddccld0XN0c.
+      ...'''.,,,,,';c;;;,,,,;;;,,;;,:;;;;;;;;;;:::::::;'o;.;:::;,''''.....'':::cldc'....
+      ..,'',',,,,,.'''..         ....                  ...         .' co:c,.....ccclllccll,...    .......
+      .'::;:,;...........','.'...'::,'...'..'....',,''',''..........'.,'...    'ooooddddol........c,ocoodx:c,.
+     .loxccl,c.':cl:,,,,,d,'.c,;:,;,:;,.:;c,c.,;,lc:lo:ld:,,'.......';;;'......'',;;::clxOx0W0kN0lxo:kXKNNxocc.
+     .cxoccldxXX0OOK0x:::x..:O;:c::cc,;::,'c:,:c;xdOkk0xO:,,,,,;;o0XKO0KKKkl;,;c:lloxd:l0kldNdlXdoKXKMXWNX0dxd
+    '::lll::dN0kk0kdd00:cxccd0;:c:;;;c::::,lc:::;kokKO0dKl;;;;;:0NOkkxdooookOoccccclllod0KOKXK0XK0KK0K00000OO,
+    .:ooll':0KkO0kdxOkNx:,,,,,:cllccloccc'l::ll::0X0xxOKNc;;;;;OXxkdoOkX0O0Oxdl::cccccccclllclooolllllolllccll;o.
+      '::c:c0Oxk0dlckkNNlllllloooooooooolooocodllkNXXXXNNxc::;cXxOod00koxkdK0Kdl::::ccllodxl;:odkkkkkOOOO0OOO0,.
+           .cx;o0dllkOWMdllc;',:;;;:::::;::c:c;;;:ccloodoocc::oKKdlkKxddlolxOKOccc:coodk0XOONOkkOk0MMMMMMNkkdo.
+             d;;oOOOOKMMWW0.........''':xxxdxxoll:;,,cccccc;';dXNd;x0kxololOkKx,clllolclodddxdddddk0XXXKK0doc;;'
+              do;,:dKMMMWx              .:odxxd:.          ...'l0Nc;xXOXO00KKx,ooooodddooodxkkOOO00KKo...
+               ;dk0K0kd:.                                        kWk::oxkkxl:cKMWK'...  .cx0NWWNXKOo.
+                                                                  ;0WXOxddx0NMM0;
+                                                                    .;lxkOkxl,.
+""")
+
+
+
+print("""
+                  .....                  .'..
+                .'.';loo:             .;::ccl:'
+               .'.,..,,:d:'''',,''''..;'..'..:l,.
+            .';'.;'  .,;;;,,,''''.'''''''.  .,,l,..
+          .,'c,.'co:::;;;;,,,''''........''','''o,.'.
+         .;'cc'.,:cclll;;;;;,''''.........''''' 'c'''
+         :':;;''oooooooc;;,,,',,'..''.....''''''.l'',.
+        ;,,.:;.,dxoooool,,,,,'''''''........''''':,.,'.
+        c'' l,.:xxdoollc;,,,,,,,,.............'''::..:'
+       ,c,..l''dddoccc:;;;,',',,,'.'..''''..''''''o'.;,.
+     .;dl,;l:':odxkOkxdol;'''',;:;;;;:;;;;,,',''''cc.:;,.
+    ;:xOl,;o;'cxX0xo:;;;,'''',;,'',,,,:c:lllol:''',o'::,;,.
+   .llkdo,,x;,ooc;,'''''''''.........lkkllxok0NNO:,l;;;',:'
+   ::dodo;'k:;,,,,,,,'''.'''...',,..lodddc:;cdNWNXooc:;..''
+   l:xloo;'kc,,',;c:;,,'..'':x::;.''.;cc;,,,'';OXKKxc:,,.',
+   dolcoo:.dl;,;cdkxl:;,,,,o0Oc:;...''c:;;;;;:;;xK0k;;;, ;c.
+   od;locc.co;o0KKxc',..'',lxl;;;...'''''..',,';;;ll'':, 'l'
+   oolco;c,cx;k00KO:,,,,,;;ld:,,'..'''...,','',,''':,'c,.':,
+   c;:co,lxld,oO0xo:;,,;;::lc;,'''..'''...'''...'':;,,c,..;.
+   ,l:;d'ccdo'xX0xd:;;;;:ldkdo::;,,,,,,,''....''',lc,':;.',
+   .dk;k,ccxolNWK0Odc;,,l0X0kdcl:,;;;;c:;,,'..',;;;d;'::,c.
+   ;ok.O;oxOkOWN0K0kocc,:l'',','.',,,;lc:'',,;,,,;;;;':c.c.
+   lk:'kco0k0KKk0K0OOxdxdxxl,,,,',..'',;'',;:;'''.;lc,:;.;,
+   oO :d:d0dO0KK0000Od00kOOo;,,,''''''.'.';:;''''',:c,c;..,
+   ll :o:''ldd0XKK000OXX0kkoc,,''''''..''',;'''''.,:,.;:..;
+   co.;l;' ,ddoxO0KKKNNNXKxc:,,'''''...'',:c;,''..;c .'o. c.
+   ;d  ,;, .olc:cdOXNXNNNO:.....     ...'::,''....,; ,'o. .'
+   'x  .:,  loc:;;:lkXOx,.....      ..   .,'.....':. :,;.
+   .d   :;  col::;,''o,............,;;;'.  ,.....,:   ::.
+   ,l   .:  :dolco;'.:  .,         .clc:'  .....';;    l.
+  .cd   ';. ;doddxx:.' .;'          ;lcc,. . ..',;;    ..
+   :   .:;  .oodok0x.' ':'          ;llc:. .',',;;'
+        '.   oodokNk;c',c;         .cclcoo',;c;,;;.
+             ;oodkNx:l';:;'        .;cccld',,;;,,,
+              olox0kod';c            'c:lo',';;;,.
+              .ldkkKKc',,             .;o;',;;;;,
+               .oxkKNo'';.             'l'',::;;
+                 .,cc;''',             :;.....
+
+""")
+
+
+print("""
+                                             .'co;..
+                                            .:. .'oOK0kdl:,'..
+                                            .:'..d0KNXXXXXXXXXXK0Okdl,
+                                            O0o: :0KKKKKKXXXXXXXXXXXKK
+                                           .Od. .k0c.,c;,::cd0KXNNNXXK'
+                                           ;  ..o00d. ,:dloc'.'d0KKKKK:
+                                        ..'Od.l0OkO0x..0XXXXXO, ;O000Od
+                                       .ko ;KNXO,'';:;..lkKXXXK; :O0koc.
+                                    .; .OO:,loc,''''''''..';cc:'..',''',
+                                 ', l0:,c:,'''''''''''''''''''''''''''',
+                           . .k. xk.;;''''''''''.......''c''''''''''''''.
+                       .: :x..o;;,,'''''''.............''k0d:'''''''''''.
+                    ...,,.','',,''''............'''''''''xXXXk:'''''''''.
+                   ..''''':loxkOOOO00Okxdolc:;,'........;0XXXXXx,'''''''.
+    ':,,;;;;,,,;;;;'...'''''''',;::ldk0KXXXXNNNNXK0Okkk0XXXXXXXXd'''''''.
+   'kOOOOOOOOOOkkkkkxccdxdd;''''''''''',;cox0XNNNNNXXXXXXXXXXXXXo'''''''.
+  .'codxkO0000OOOOOOOOO000O0o,'''''''''''''''';:lox0KK0Okkxxdddo;''''''',
+  c:,.......''',,;;;;;:::::::;.''''''''''''''''''''''''''''''''''''''''''
+  'd00kxdlc;,'.............''''..........................................
+""")
